ec0f60820a69c4d0bc6e8904528abe3414bca0d3
pw-wsb <101039960+pw-wsb@users.noreply.github.com>
2022-04-04 18:07:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    """
-    Function return h1 tag at endpoint 127.0.0.1:5000/
-    """
     return "<h1>Hello WSB! Greetings from Flask & Docker!<h1>"
 
 
